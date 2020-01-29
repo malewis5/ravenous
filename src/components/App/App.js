db0d48dd7d961605e1ca9195fa068f64d12e999b
Matthew Lewis <matthewlewis@Matthews-MacBook-Pro.local>
2020-01-29 12:48:50 -0600

    check

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+import { render } from "@testing-library/react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
