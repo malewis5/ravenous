c542eedd6518ce11e29fe5c1052d613f81dbca51
Lewis <malewis5@cba.ua.edu>
2020-02-03 15:08:20 -0600

    Fixed Yelp

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,19 +1,17 @@
-import React from "react";
-import "./BusinessList.css";
-import Business from "../Business/Business";
+import React from 'react';
+import './BusinessList.css';
+
+import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
   render() {
     return (
       <div className="BusinessList">
-        {this.props.businesses.map(business => {
-          return <Business business={business} />;
-        })}
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} key={business.id} />
+          })
+        }
       </div>
     );
   }
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,30 +1,33 @@
 const apiKey =
-  "Fx46kldzklg18cn4dejUr3jdgoDCrX1RO6UWrzayDjwy2wonCi5oM_U6b5OR-RxTnGWElq0MxtGQdT6NvxCOwvc8BucEsWauvZWi3oyYyA_f6z5HwXZHMATbqVc4XnYx";
+  "Fx46kldzklg18cn4dejUr3jdgoDCrX1RO6UWrzayDjwy2wonCi5oM_U6b5OR-RxTnGWElq0MxtGQdT6NvxCOwvc8BucEsWauvZWi3oyYyA_f6z5HwXZHMATbqVc4XnYx"; // Insert API key here.
 
 const Yelp = {
   search(term, location, sortBy) {
     return fetch(
       `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-      { headers: { Authorization: `Bearer ${apiKey}` } }
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
+      }
     )
-      .then(response => response.json())
+      .then(response => {
+        return response.json();
+      })
       .then(jsonResponse => {
         if (jsonResponse.businesses) {
-          return jsonResponse.business.map(business => {
-            console.log(business);
-            return {
-              id: business.id,
-              imageSrc: business.image_url,
-              name: business.name,
-              address: business.location.address1,
-              city: business.location.city,
-              state: business.location.state,
-              zipCode: business.location.zip_code,
-              category: business.categories[0].title,
-              rating: business.rating,
-              reviewCount: business.review_count
-            };
-          });
+          return jsonResponse.businesses.map(business => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }));
         }
       });
   }
