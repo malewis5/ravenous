cf235a2e85053529b870fd9724ed5cc12e460aef
Lewis <malewis5@cba.ua.edu>
2020-02-03 14:17:29 -0600

    Finished Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,31 +3,31 @@ const apiKey =
 
 const Yelp = {
   search(term, location, sortBy) {
-    // Fetch API
     return fetch(
       `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-      {
-        headers: {
-          Authorization: `Bearer ${apiKey}`
-        }
-      }
+      { headers: { Authorization: `Bearer ${apiKey}` } }
     )
       .then(response => response.json())
       .then(jsonResponse => {
         if (jsonResponse.businesses) {
-          return jsonResponse.businesses.map(business => {
+          return jsonResponse.business.map(business => {
+            console.log(business);
             return {
               id: business.id,
               imageSrc: business.image_url,
               name: business.name,
-              address: business.address,
-              city: business.city,
-              state: business.state,
-              zipCode: business.zip_code,
-              category: business.category
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
             };
           });
         }
       });
   }
 };
+
+export default Yelp;
