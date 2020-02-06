1385a607d0f371f2f0713171749b9068e88ba2a3
Lewis <malewis5@cba.ua.edu>
2020-02-06 09:22:38 -0600

    Updates

---
+++ precache-manifest.14c93f744fd119e76f7bc96900cf6191.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6a176e1ebb2b14923bf9a33f0b144d",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "d8e4c33a84057b167e0f",
+    "url": "/ravenous/static/css/main.fef6321c.chunk.css"
+  },
+  {
+    "revision": "53c9c50865f1319ae46e",
+    "url": "/ravenous/static/js/2.74922d7a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ravenous/static/js/2.74922d7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8e4c33a84057b167e0f",
+    "url": "/ravenous/static/js/main.ba805b40.chunk.js"
+  },
+  {
+    "revision": "6daa093969d3829a08d5",
+    "url": "/ravenous/static/js/runtime-main.af183450.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
